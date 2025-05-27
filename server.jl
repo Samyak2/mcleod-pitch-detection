a8bafe30772f6ddaca9adefeb6e6d206b6173058
Samyak S Sarnayak <samyak201@gmail.com>
2025-05-27 20:56:38 +0530

    Listen server on all addresses

--- server.jl
+++ server.jl
@@ -3,4 +3,8 @@ Pkg.activate(".")
 
 using PlutoSliderServer
 
-PlutoSliderServer.run_notebook("notebooks/index.jl")
+PlutoSliderServer.run_notebook(
+    "notebooks/index.jl";
+    SliderServer_host="0.0.0.0",
+    SliderServer_port=2345,
+)
